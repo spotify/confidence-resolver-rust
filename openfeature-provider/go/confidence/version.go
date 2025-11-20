dd185507b2406c65e9d79d10aea91759cc391d0d
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2025-11-20 08:57:52 +0100

    fix: release please for go version
    Co-authored-by: Nicklas Lundin <nicklasl@spotify.com>

--- openfeature-provider/go/confidence/version.go
+++ openfeature-provider/go/confidence/version.go
@@ -2,4 +2,4 @@ package confidence
 
 // Version is the current version of the Confidence OpenFeature provider.
 // This version is updated automatically by release-please.
-const Version = "0.1.0"
+const Version = "0.1.0" // x-release-please-version
